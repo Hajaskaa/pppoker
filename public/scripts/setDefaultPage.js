abb3411cd075854cc43a4b7a35a2a78691414960
Relatyvity <relatyvity@gmail.com>
2023-12-07 22:06:36 +0100

    12/7 fix set default page

--- public/scripts/setDefaultPage.js
+++ public/scripts/setDefaultPage.js
@@ -3,6 +3,6 @@ import { changePage } from "./util.js";
 export default function setDefaultPage(socket) {
   socket.on("setDefaultPage", () => {
     console.log("Default page set");
-    changePage("1");
+    changePage("page1");
   });
 }
